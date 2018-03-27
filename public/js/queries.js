510cfdd06754a599c0549d5121aabe75ce9c0214
jeffzfeng <jefff.feng@mail.utoronto.ca>
2018-03-27 18:58:54 -0400

    added creds for test private store

---
+++ public/js/queries.js
@@ -0,0 +1,67 @@
+var storeFrontAPI = "https://snake-game-store.myshopify.com/api/graphql";
+var storeFrontAccessToken = "b3d3605b74d0def71ac2c6fcf90d4353";
+
+// Sets up the GraphQL request headers
+function makeRequest(query) {
+  var headers = {
+    "X-Shopify-Storefront-Access-Token": storeFrontAccessToken,
+    "Content-Type": "application/json"
+  };
+
+  return $.ajax({
+    url: storeFrontAPI,
+    type: "POST",
+    data: JSON.stringify({ query: query }),
+    headers: headers
+  });
+}
+
+// Queries for product information
+function fetchProducts() {
+  var query = `
+
+  `;
+
+  return makeRequest(query);
+}
+
+// // Buys new power up by creating new checkout with item
+// function buyPowerUp(variantId) {
+//   var query = `
+//     mutation {
+//       checkoutCreate() {
+//
+//         }
+//       }
+//     `;
+//
+//   return makeRequest(query);
+// }
+//
+// Checks completed purchases by querying checkouts with `completedAt` value
+function checkCompletedPurchases(checkoutIds) {
+  var query = `
+      query {
+        nodes(ids: [${checkoutIds}]) {
+          ... on Checkout {
+            id
+            completedAt
+            lineItems(first: 3) {
+              edges {
+                node {
+                  variant {
+                    id
+                    product {
+                      handle
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
+  return makeRequest(query);
+}
